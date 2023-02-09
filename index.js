0c243a4f1c9ab808b438d83da301b40b3041db3f
Ayelen Khoury <ayelenkhoury@gmail.com>
2023-02-08 22:16:19 -0300

    terminado ejercicio 2

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-// 1)- Variable de tipo constante cuyo valor sea el siguiente mensaje “Fin del Ejercicio”
+// 1)- Crear una variable de tipo constante cuyo valor sea el siguiente mensaje “Fin del Ejercicio”
 
 const finDelEjercicio = "Fin del Ejercicio";
 console.log(finDelEjercicio);
+
+// 2)- Declarar 5 variables de tipo string y concatenarlas en diferente orden
+
+let saludo = "Hola mundo! ";
+let enunciado = "Este es un ejercicio que consiste en ";
+let ejercicio = "concatenar variables en diferente orden ";
+let condicion = "sin importar que la oración formada no tenga sentido ";
+let despedida = "eso es todo. ¡Saludos!"
+console.log(saludo + ejercicio + despedida + enunciado + condicion);
